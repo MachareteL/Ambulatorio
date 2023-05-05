d939c8968be957cbea1f4527fc79f965d88fbede
MachareteL <macharetelucas@gmail.com>
2023-05-05 07:50:43 -0300

    basic components just DONE

--- src/components/Funcionario.tsx
+++ src/components/Funcionario.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { PoppyTitulo } from "@/pages";
 import BtnNext from "./BtnNext";
 import { TextField } from "@mui/material";
@@ -8,16 +8,17 @@ type FuncionarioType = {
 }
 
 export default function Funcionario({goToNext}: FuncionarioType) {
+  const [valido, setValido] = useState(true)
   return (
     <div className="w-10/12 m-auto  h-5/6 mt-10 flex flex-col justify-between">
       <div className="flex flex-col gap-12">
         <h1 className={`${PoppyTitulo.className} text-3xl place-self-center [word-spacing: 2rem]`}>
           Dados do Colaborador
         </h1>
-        <TextField label="EDV" placeholder="00000000" type={'number'}/>
+        <TextField label="EDV" placeholder="00000000" type={'number'} />
         <TextField label="Nome" placeholder="Digite seu nome completo" />
       </div>
-      <BtnNext onClick={goToNext} />
+      <BtnNext onClick={goToNext} valid={valido}/>
     </div>
   );
 }
